e4c53659f2cc453b1f91164bec01762e3e6430a9
Italo Thiago <italothiago220@gmail.com>
2023-09-01 12:02:48 -0300

    inicio projeto pokedex

---
+++ JavaScript/Introdução ao Desenvolvimento Web com JavaScript/Pokedex/script.js
@@ -0,0 +1,4 @@
+const btnTeste = document.getElementById("btnTeste");
+btnTeste.addEventListener("click", function () {
+  console.log("Clickou no botão!")
+})
---
+++ JavaScript/Sintaxe Básica em JavaScript/aula 2 - Variavéis/script.js
@@ -0,0 +1,99 @@
+// tipos primitivos
+
+// boolean
+// var vOuF = false;
+// console.log(typeof(vOuF));
+
+// number
+// var numeroQualquer = 1;
+// console.log(typeof(numeroQualquer));
+
+// string
+// var nome = "Italo"
+// console.log(typeof(nome));
+
+//function
+// function funcao() { }
+// console.log(typeof(funcao));
+
+// como declarar
+// var variavel = "Italo";
+// variavel = "Thiago";
+// console.log(variavel);
+
+// let variavel2 = "Thiago";
+// variavel2 = "Italo";
+// console.log(variavel2);
+
+// const constante = "Silva";
+//constante = 'Gomes';
+// console.log(constante);
+
+// var escopoGlobal = "Global";
+// console.log(escopoGlobal);
+
+// function escopoLocal() {
+//   let escopoLocalInterno = "Local";
+//   console.log(escopoLocalInterno);
+// }
+// escopoLocal();
+
+// atribuição
+// var atribuicao = "Italo";
+
+// comparação
+// var comparacao = "0" == 0;
+// console.log(comparacao);
+
+// comparação identica
+// var comparacaoIdentica = "0" === 0;
+// console.log(comparacaoIdentica);
+
+// // adição
+// var adição = 1 + 1;
+// console.log(adição);
+
+// // subtração
+// var subtração = 2 - 1;
+// console.log(subtração);
+
+// // multiplicação
+// var multiplicação = 2 * 3;
+// console.log(multiplicação);
+
+// // divisão real
+// var divisãoReal = 6 / 2;
+// console.log(divisãoReal);
+
+// // divisão inteira
+// var divisãoInteira = 5 % 2;
+// console.log(divisãoInteira);
+
+// // potenciação
+// var potenciação = 2 ** 10;
+// console.log(potenciação); 
+
+// // maior que
+// var maiorQue = 5 > 2;
+// console.log(maiorQue); 
+
+// // menor
+// var menorQue = 5 < 2;
+// console.log(menorQue);
+
+// // maior ou igual a 
+// var maiorOuIgual = 5 >= 2;
+// console.log(maiorOuIgual);
+
+// // menor ou igual a
+// var menorOuIgual = 5 <= 2;
+// console.log(menorOuIgual);
+
+// var e = true && true;
+// console.log(e);
+
+// var ou = false || false;
+// console.log(ou);
+
+// var nao = !true;
+// console.log(nao);
